8bba520569c45d0dcc59a8e50916af26d4f8899d
aymane <houri.aymane03@gmail.com>
2024-11-10 21:52:54 +0100

    Activation de scheduling dans l'application principale et mise à jour de SqsConfig

--- src/main/java/fplarache/smartlib/notifications/configuration/SqsConfig.java
+++ src/main/java/fplarache/smartlib/notifications/configuration/SqsConfig.java
@@ -1,5 +1,7 @@
 package fplarache.smartlib.notifications.configuration;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.regions.Region;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,13 +11,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SqsConfig {
 
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretAccessKey}")
+    private String secretAccessKey;
+
     @Value("${aws.region}")
-    private String region;
+    private String region ;
 
     @Bean
     public SqsClient sqsClient() {
         return SqsClient.builder()
                 .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+                ))
                 .build();
     }
 }
